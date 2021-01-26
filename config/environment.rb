946ec2dd361e0c55ae0876f727e913c1844658f4
Wellington Santos <wjsantos.wj@gmail.com>
2021-01-26 18:14:39 -0300

    config/environment: changing the way we connecto to database to easily plug activerecord

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,18 @@ require 'rubygems'
 require 'bundler'
 
 APP_ENV = ENV.fetch('RACK_ENV', 'development').freeze
-
 Bundler.require(:default, APP_ENV)
 
-if APP_ENV != 'development'
+require 'active_record'
+
+if APP_ENV != 'production'
   require 'dotenv/load'
 end
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'postgresql',
+  url:     ENV.fetch('DATABASE_URL')
+)
+
 
 require_relative '../lib/cs_managers'
