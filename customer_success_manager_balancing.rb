4ccf08d76f4d7486d7cf9208e8e9cca33b25f52c
Wellington Santos <wjsantos.wj@gmail.com>
2021-01-26 13:35:19 -0300

    cs_manager_balancing: formating specs for a better code readibility. Must respecting 80col line length

--- customer_success_manager_balancing.rb
+++ customer_success_manager_balancing.rb
@@ -17,8 +17,20 @@ end
 
 class CustomerSuccessManagersBalancingTests < Minitest::Test
   def test_scenario_one
-    css = [{ id: 1, score: 60 }, { id: 2, score: 20 }, { id: 3, score: 95 }, { id: 4, score: 75 }]
-    customers = [{ id: 1, score: 90 }, { id: 2, score: 20 }, { id: 3, score: 70 }, { id: 4, score: 40 }, { id: 5, score: 60 }, { id: 6, score: 10}]
+    css = [
+      { id: 1, score: 60 },
+      { id: 2, score: 20 },
+      { id: 3, score: 95 },
+      { id: 4, score: 75 }
+    ]
+    customers = [
+      { id: 1, score: 90 },
+      { id: 2, score: 20 },
+      { id: 3, score: 70 },
+      { id: 4, score: 40 },
+      { id: 5, score: 60 },
+      { id: 6, score: 10}
+    ]
 
     balancer = CustomerSuccessManagersBalancing.new(css, customers, [2, 4])
     assert_equal 1, balancer.execute
@@ -37,35 +49,57 @@ class CustomerSuccessManagersBalancingTests < Minitest::Test
 
     customers = Array.new(10000, 10)
 
-    balancer = CustomerSuccessManagersBalancing.new(array_to_map(cs_managers), array_to_map(customers), [1000])
+    balancer = CustomerSuccessManagersBalancing.new(
+      array_to_map(cs_managers),
+      array_to_map(customers),
+      [1000]
+    )
 
     result = Timeout.timeout(1.0) { balancer.execute }
     assert_equal 999, result
   end
 
   def test_scenario_four
-    balancer = CustomerSuccessManagersBalancing.new(array_to_map([1, 2, 3, 4, 5, 6]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [])
+    balancer = CustomerSuccessManagersBalancing.new(
+        array_to_map([1, 2, 3, 4, 5, 6]),
+        array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        []
+    )
     assert_equal 0, balancer.execute
   end
 
   def test_scenario_five
-    balancer = CustomerSuccessManagersBalancing.new(array_to_map([100, 2, 3, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [])
+    balancer = CustomerSuccessManagersBalancing.new(
+      array_to_map([100, 2, 3, 3, 4, 5]),
+      array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
     assert_equal balancer.execute, 1
   end
 
   def test_scenario_six
-    balancer = CustomerSuccessManagersBalancing.new(array_to_map([100, 99, 88, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [1, 3, 2])
+    balancer = CustomerSuccessManagersBalancing.new(
+      array_to_map([100, 99, 88, 3, 4, 5]),
+      array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [1, 3, 2]
+    )
     assert_equal balancer.execute, 0
   end
 
   def test_scenario_seven
-    balancer = CustomerSuccessManagersBalancing.new(array_to_map([100, 99, 88, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [4, 5, 6])
+    balancer = CustomerSuccessManagersBalancing.new(
+      array_to_map([100, 99, 88, 3, 4, 5]),
+      array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [4, 5, 6]
+    )
     assert_equal balancer.execute, 3
   end
 
   def array_to_map(arr)
     out = []
-    arr.each_with_index { |score, index| out.push({ id: index + 1, score: score }) }
+    arr.each_with_index do |score, index|
+      out.push({ id: index + 1, score: score })
+    end
     out
   end
 end
